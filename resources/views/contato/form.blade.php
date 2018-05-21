d93758d49540d1b133cd58f057c58317e603d921
Gabriel Henrique <gabrielhs.96@hotmail.com>
2018-05-21 16:03:51 -0300

    Correcao informacoes duplicadas

--- resources/views/contato/form.blade.php
+++ resources/views/contato/form.blade.php
@@ -22,19 +22,19 @@
                        <input type="hidden" name="id" value="{{ $contato->id }}">
                         <div class="form-group">
                             <label for="name">Nome</label>    
-                            <input type="text" name="name" id="name" class="form-control" value="{{ $contato->name }}{{ old('name') }}">
+                            <input type="text" name="name" id="name" class="form-control" value="{{ $contato->name }}">
                         </div>
                         <div class="form-group">
                             <label for="email">E-mail</label>    
-                            <input type="email" name="email" id="email" class="form-control" value="{{ $contato->email }}{{ old('email') }}">
+                            <input type="email" name="email" id="email" class="form-control" value="{{ $contato->email }}">
                         </div>
                         <div class="form-group">
                             <label for="phone_number">Telefone</label>    
-                            <input type="tel" name="phone_number" id="phone_number" class="form-control" maxlength="15" value="{{ $contato->phone_number }}{{ old('phone_number') }}">
+                            <input type="tel" name="phone_number" id="phone_number" class="form-control" maxlength="15" value="{{ $contato->phone_number }}">
                         </div>
                         <div class="form-group">
                             <label for="birthdate">Data de Nascimento</label>    
-                            <input type="date" name="birthdate" id="birthdate" class="form-control" value="{{ $contato->birthdate }}{{ old('birthdate') }}">
+                            <input type="date" name="birthdate" id="birthdate" class="form-control" value="{{ $contato->birthdate }}">
                         </div>
                         <div class="form-group">
                             <input type="submit" value="Salvar" class="btn btn-success">
